80e6f93298ca555d043bba1d130facc5dc8d2d48
theo <lesgooo1993@proton.me>
2024-03-21 21:52:56 +0530

    deleted old svelte files; initialized sveltekit project; moved api.js and store.js to src/lib; moved ui/public/assets to ui/static/assets

--- ui/src/store.js
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const messages = writable([]);
-export const projectList = writable([]);
-export const modelList = writable([]);
-export const agentState = writable(null);
-export const internet = writable(true);
