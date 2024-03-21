0557bcfa91686c814c5ac23833dffb033bb90c15
Vasanth <vasanthp.cse2019@citchennai.net>
2024-03-21 17:27:56 +0530

    Groq Added

--- src/config.py
+++ src/config.py
@@ -22,6 +22,9 @@ class Config:
     def get_netlify_api_key(self):
         return self.config["API_KEYS"]["NETLIFY"]
     
+    def get_groq_api_key(self):
+        return self.config["API_KEYS"]["GROQ"]
+    
     def get_sqlite_db(self):
         return self.config["STORAGE"]["SQLITE_DB"]
     
