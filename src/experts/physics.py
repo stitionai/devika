daa757e173feed4e469fdbaeb69058cd133085b3
Gautam Sharma <gautam@zibtek.in>
2024-03-22 16:04:22 +0530

    Add PhysicsFunctions class with calculate_force, calculate_velocity, and calculate_displacement methods

--- src/experts/physics.py
+++ src/experts/physics.py
@@ -1,3 +1,13 @@
-"""
-Physics Function Calls
-"""
+class PhysicsFunctions:
+    def __init__(self):
+        pass
+
+    def calculate_force(self, mass, acceleration):
+        return mass * acceleration
+
+    def calculate_velocity(self, initial_velocity, acceleration, time):
+        return initial_velocity + acceleration * time
+
+    def calculate_displacement(self, initial_velocity, time, acceleration):
+        return initial_velocity * time + 0.5 * acceleration * time ** 2
+
