14f4c9ff41d3dc4ab5c2bdbd9fd19137cf428c41
Gautam Sharma <gautam@zibtek.in>
2024-03-22 16:09:56 +0530

    Refactor ChemistryExpert class to include methods for parsing SMILE notation, getting molecule properties, and visualizing molecules

--- src/experts/chemistry.py
+++ src/experts/chemistry.py
@@ -1,11 +1,24 @@
-"""
-Function calls and Parser for:
-- SMILE Notation
-- Molecule Parser
+from rdkit import Chem
+from rdkit.Chem import Draw
 
-Visualization for:
-- Molecule Structure
-- Molecule Properties
+class ChemistryExpert:
+    def __init__(self):
+        pass
 
-Use RDKit bindings
-"""
+    def parse_smile(self, smile_notation):
+        mol = Chem.MolFromSmiles(smile_notation)
+        return mol
+
+    def get_molecule_properties(self, molecule):
+        properties = {}
+        properties['num_atoms'] = molecule.GetNumAtoms()
+        properties['num_bonds'] = molecule.GetNumBonds()
+        properties['formula'] = Chem.rdMolDescriptors.CalcMolFormula(molecule)
+        properties['molecular_weight'] = Chem.rdMolDescriptors.CalcExactMolWt(molecule)
+        return properties
+
+    def visualize_molecule(self, molecule, filename=None):
+        if filename:
+            Draw.MolToFile(molecule, filename)
+        else:
+            Draw.MolToImage(molecule).show()
