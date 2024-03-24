40ae0534909a11ca84eaeb1cb952e07f25bac1e5
rgsk <guptarahul70322@gmail.com>
2024-03-24 17:31:48 +0530

    update settings

--- ui/src/routes/settings/Settings/Settings.svelte
+++ ui/src/routes/settings/Settings/Settings.svelte
@@ -4,28 +4,46 @@
   import Button from "./Button.svelte";
   import Input from "./Input.svelte";
   let result;
+  let settingsLoading;
   onMount(async () => {
     result = await getSettings();
     console.log(result);
   });
 
   async function handleSubmit() {
+    settingsLoading = true;
     const postResult = await setSettings(result);
     console.log(postResult);
+    settingsLoading = false;
   }
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-  <p>API_KEYS</p>
+<div class="px-4 py-8 max-w-[600px]">
   {#if result}
-    <div class="pl-4">
-      <Input
-        label="CLAUDE"
-        type="text"
-        bind:value={result["API_KEYS"]["CLAUDE"]}
-      />
-    </div>
+    <form on:submit|preventDefault={handleSubmit}>
+      <div class="space-y-8">
+        {#each Object.keys(result) as mainKey}
+          <div>
+            <p class="mb-4">{mainKey}</p>
+            <div class="pl-4 space-y-3">
+              {#each Object.keys(result[mainKey]) as key}
+                <Input
+                  label={key}
+                  type="text"
+                  bind:value={result[mainKey][key]}
+                />
+              {/each}
+            </div>
+          </div>
+        {/each}
+      </div>
+      <div class="h-[20px]"></div>
+      <Button
+        type="submit"
+        text="Submit"
+        isActive={settingsLoading}
+        activeText="Submitting..."
+      ></Button>
+    </form>
   {/if}
-
-  <Button type="submit" text="Submit"></Button>
-</form>
+</div>
