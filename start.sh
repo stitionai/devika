c316ef07ab5aa3d9d776d69b5129fe5ab73b4433
vital121 <123862035+vital121@users.noreply.github.com>
2024-03-24 21:05:08 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v $(pwd).env:/devika/.env -v $(pwd)/config.toml:/devika/config.toml -v $(pwd)/default.conf:/etc/nginx/sites-available/default -p 1337:1337 -p 3001:80 devika-image:latest
+docker run -it --rm -v $(pwd)/.env:/devika/.env -v $(pwd)/config.toml:/devika/config.toml -v $(pwd)/default.conf:/etc/nginx/sites-available/default -p 1337:1337 -p 3001:80 devika-image:latest
