6eea5d14c1b65c38d24cb18506da56b8ebcdae8a
TheOnlyWayUp <thedarkdraconian@gmail.com>
2024-03-25 21:37:11 +0000

    Use adapter-static to build UI

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
