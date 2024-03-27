a4e717fd05423e3c7f02994449a226774eab7de4
vishnuxx <vishnuvs2002it@gmail.com>
2024-03-28 00:24:03 +0530

    created unified interface for search engines

--- src/browser/search.py
+++ src/browser/search.py
@@ -1,25 +1,28 @@
-import requests
-from src.config import Config
+from enum import Enum
+# from src.config import Config
+from .search_engines import BingSearch , DuckDuckGoSearch
 
-class BingSearch:
+
+class SearchEngine(Enum):
+    BING = BingSearch()
+    DUCKDUCKGO = DuckDuckGoSearch()
+
+
+class WebSearch:
     def __init__(self):
-        self.config = Config()
-        self.bing_api_key = self.config.get_bing_api_key()
-        self.bing_api_endpoint = self.config.get_bing_api_endpoint()
+        # self.config = Config()
+        self.search_engine = SearchEngine.DUCKDUCKGO.value
         self.query_result = None
 
     def search(self, query):
-        headers = {"Ocp-Apim-Subscription-Key": self.bing_api_key}
-        params = {"q": query, "mkt": "en-US"}
-
-        try:
-            response = requests.get(self.bing_api_endpoint, headers=headers, params=params)
-            response.raise_for_status()
-            self.query_result = response.json()
+        if self.search_engine == SearchEngine.BING:
+            bing_search = BingSearch()
+            self.query_result = bing_search.search(query)
             return self.query_result
-        except Exception as err:
-            return err
+        elif self.search_engine == SearchEngine.DUCKDUCKGO:
+            duckduckgo_search = DuckDuckGoSearch()
+            self.query_result = duckduckgo_search.search(query)
+            return self.query_result
 
     def get_first_link(self):
-        return self.query_result["webPages"]["value"][0]["url"]
-
+        return self.search_engine.get_first_link()
