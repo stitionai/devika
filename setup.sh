653807be0956513a1a04d40ee6de7db3db724355
franklinselva <franklinselva10@gmail.com>
2024-03-27 07:33:52 +0100

    Refactor setup.sh script to support installation in development mode

--- setup.sh
+++ setup.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-pip3 install -r requirements.txt
-playwright install
-python3 -m playwright install-deps
-cd ui/
-bun install
+# This script is used to install the project in development mode or normal mode
+function install_normal {
+    pip3 install .
+    playwright install
+    python3 -m playwright install-deps
+    cd ui/ || exit 1
+    npm install
+}
+
+function install_dev {
+    pip3 install -e ".[dev]"
+    pre-commit install
+    playwright install
+    python3 -m playwright install-deps
+    cd ui/ || exit 1
+    npm install
+}
+
+if [ "$1" == "--dev" ]; then
+    printf '\033[0;32m%s\033[0m\n' "Installing in development mode"
+    install_dev
+else
+    printf '\033[0;31m%s\033[0m\n' "Installing in normal mode"
+    install_normal
+fi
