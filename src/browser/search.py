b9df7e434f3133f51b19c2dd94fb4e8c0e5feae5
vishnuxx <vishnuvs2002it@gmail.com>
2024-03-28 14:17:44 +0530

    implemented set_search_engine() function in search.py

--- src/browser/search.py
+++ src/browser/search.py
@@ -13,16 +13,13 @@ class WebSearch:
         # self.config = Config()
         self.search_engine = SearchEngine.DUCKDUCKGO.value
         self.query_result = None
+        
+    def set_search_engine(self, search_engine : SearchEngine):
+        self.search_engine = search_engine.value
 
     def search(self, query):
-        if self.search_engine == SearchEngine.BING:
-            bing_search = BingSearch()
-            self.query_result = bing_search.search(query)
-            return self.query_result
-        elif self.search_engine == SearchEngine.DUCKDUCKGO:
-            duckduckgo_search = DuckDuckGoSearch()
-            self.query_result = duckduckgo_search.search(query)
-            return self.query_result
+        self.query_result = self.search_engine.search(query)
+        return self.query_result
 
     def get_first_link(self):
         return self.search_engine.get_first_link()
