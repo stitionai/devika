ed7b63ed303d2b68bee9c89316165d1ef7e40771
franklinselva <franklinselva10@gmail.com>
2024-03-29 21:02:02 +0100

    Refactor Ollama client to support multiple models

--- src/llm/ollama_client.py
+++ src/llm/ollama_client.py
@@ -1,31 +1,65 @@
+"""Ollama client for interacting with Ollama API."""
+
+from typing import Dict
 import httpx
-from ollama import Client
+from ollama import Client  # type: ignore
 
 from src.config import Config
-from src.logger import Logger
+from .base import BaseLLMModel
+from .exception import (
+    ModelNotSupported,
+    LLMException,
+    LLMConfigException,
+)
 
-logger = Logger()
 
-client = Client(host=Config().get_ollama_api_endpoint())
+class Ollama(BaseLLMModel):
+    """Ollama client for interacting with Ollama API."""
 
+    SUPPORTED_MODELS: Dict[str, str] = {}
 
-class Ollama:
-    @staticmethod
-    def list_models():
+    def __init__(self, model_id: str, timeout: int = 30) -> None:
+        config = Config()
+        api_endpoint = config.get_ollama_api_endpoint()
+        if not api_endpoint:
+            raise LLMConfigException("Ollama API endpoint not configured.")
+
+        self._client = Client(host=api_endpoint)
+        # Check if ollama server is running
+
         try:
-            return client.list()["models"]
-        except httpx.ConnectError:
-            logger.warning(
+            self._client.list()
+        except httpx.ConnectError as e:
+            raise LLMException(
                 "Ollama server not running, please start the server to use models from Ollama."
-            )
-        except Exception as e:
-            logger.error(f"Failed to list Ollama models: {e}")
-        return []
+            ) from e
 
-    def inference(self, model_id: str, prompt: str) -> str:
+        # Get supported models
+        self._process_models_info()
+
+        if model_id not in self.SUPPORTED_MODELS:
+            raise ModelNotSupported(
+                f"Model '{model_id}' is not supported. Supported models: {self.SUPPORTED_MODELS.keys()}"  #  pylint: disable=line-too-long
+            )
+
+        self._model_id = model_id
+        self._model_name = self.SUPPORTED_MODELS[model_id]
+        self._timeout = timeout
+
+    def _process_models_info(self) -> None:
+        for model in self._client.list()["models"]:
+            model_name = model["name"].split(":")[0]
+            # model_id = model["name"] + "-" + model["details"]["parameter_size"]
+            model_id = model["name"]
+
+            self.SUPPORTED_MODELS[model_id] = model_name
+
+    def _inference(self, prompt: str) -> str:
         try:
-            response = client.generate(model=model_id, prompt=prompt.strip())
+            response = self._client.generate(model=self.model_id, prompt=prompt.strip())
             return response["response"]
         except Exception as e:
-            logger.error(f"Error during model inference: {e}")
-        return ""
+            raise LLMException("Error during model inference.") from e
+
+    def supported_models(self) -> dict:
+        return self.SUPPORTED_MODELS
