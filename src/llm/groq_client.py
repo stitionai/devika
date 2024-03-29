9d1828715f2fb182b35ca1b56f49ab28896b3797
franklinselva <franklinselva10@gmail.com>
2024-03-29 20:11:44 +0100

    Refactor Groq client for interacting with Groq API

--- src/llm/groq_client.py
+++ src/llm/groq_client.py
@@ -1,17 +1,34 @@
+"""Groq client for interacting with Groq API."""
+
 from groq import Groq as _Groq
 
 from src.config import Config
+from .base import BaseLLMModel
+from .exception import ModelNotSupported, TokenUsageExceeded
 
 
-class Groq:
-    def __init__(self):
+class Groq(BaseLLMModel):
+    """Groq client for interacting with Groq API."""
+
+    SUPPORTED_MODELS = {
+        "mixtral-8x7b-32768": "GROQ Mixtral",
+        "llama2-70b-4096": "GROQ LLAMA2-70B",
+        "gemma-7b-it": "GROQ GEMMA-7B-IT",
+    }
+
+    def __init__(self, model_id: str, timeout: int = 30):
+
+        if model_id not in self.SUPPORTED_MODELS:
+            raise ModelNotSupported(f"Model {model_id} is not supported.")
+
+        self._model_id = model_id
+        self._model_name = self.SUPPORTED_MODELS[model_id]
+
         config = Config()
         api_key = config.get_groq_api_key()
-        self.client = _Groq(
-            api_key=api_key,
-        )
+        self.client = _Groq(api_key=api_key, timeout=timeout)
 
-    def inference(self, model_id: str, prompt: str) -> str:
+    def _inference(self, prompt: str) -> str:
         chat_completion = self.client.chat.completions.create(
             messages=[
                 {
@@ -19,7 +36,13 @@ class Groq:
                     "content": prompt.strip(),
                 }
             ],
-            model=model_id,
+            model=self.model_id,
         )
 
+        if chat_completion.choices[0].finish_reason in ["max_tokens", "length"]:
+            raise TokenUsageExceeded("Token usage exceeded for Groq.")
+
         return chat_completion.choices[0].message.content
+
+    def supported_models(self) -> dict:
+        return self.SUPPORTED_MODELS
