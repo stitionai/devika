51543988c073cb61ec067e7d29f437f3238a391c
franklin selva <franklinselva10@gmail.com>
2024-03-31 09:20:49 +0200

    Add SentenceBert class and modify extract_keywords method (#4)

---
+++ devika/bert/__init__.py
@@ -0,0 +1,1 @@
+from .sentence import SentenceBert
--- devika/bert/sentence.py
+++ devika/bert/sentence.py
@@ -1,14 +1,25 @@
+"""Keyword extraction using BERT."""
+
+# pylint: disable=too-few-public-methods
+
+from typing import List, Tuple
+
 from keybert import KeyBERT
 
 
 class SentenceBert:
-    def __init__(self, sentence: str):
-        self.sentence = sentence
+    def __init__(self):
         self.kw_model = KeyBERT()
 
-    def extract_keywords(self, top_n: int = 5) -> list:
+    def extract_keywords(
+        self, sentence: str, top_n: int = 5
+    ) -> List[Tuple[str, float]]:
+
+        if isinstance(sentence, list):
+            sentence = " ".join(sentence)
+
         keywords = self.kw_model.extract_keywords(
-            self.sentence,
+            sentence,
             keyphrase_ngram_range=(1, 1),
             stop_words="english",
             top_n=top_n,
