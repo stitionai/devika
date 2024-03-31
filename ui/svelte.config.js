c1b50b9485fe2244bd902fd919c52adf60260693
franklinselva <franklinselva10@gmail.com>
2024-03-31 19:11:00 +0200

    Update Svelte configuration and Dockerfiles setup

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,5 +1,5 @@
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
