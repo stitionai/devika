bf5c2efa994cb634d351a4164577712bae80909a
rajakumar05032000 <rajakumar05032000@gmail.com>
2024-03-31 15:19:29 +0530

    Fix - optimized UI dockerfile and removed unnecessary index.html file

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,5 +1,6 @@
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-auto";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
