f1206726dbbefc53ddbb5734f6e6fd2114c14acb
Dobromir Popov <dobromir.popov@gateway.one>
2024-04-02 00:21:35 +0300

    merge, option to define ollama server URL and log served models in debug console; typo fixed

--- src/llm/ollama_client.py
+++ src/llm/ollama_client.py
@@ -1,17 +1,32 @@
 import httpx
 from ollama import Client
 from src.config import Config
-
 from src.logger import Logger
 
 client = Client(host=Config().get_ollama_api_endpoint())
 
-
 class Ollama:
-    @staticmethod
-    def list_models():
+    _ollama_client = None
+    @classmethod
+    def configure_ollama_client(self):
+        config = Config()
+        ollama_base = config.get_llm_endpoint_ollama()
+        if ollama_base is not None:
+            Logger().info(f"Using Ollama server at {ollama_base}")    
+            self._ollama_client = ollama.Client(host=ollama_base)
+        else:
+            Logger().info(f"Using Ollama server at localhost")    
+            self._ollama_client = ollama
+
+    @classmethod
+    def list_models(self):
+        # if ollama_base is None - use ollama, else use the provided ollama_base by creating new client 
+        if self._ollama_client is None:
+            self.configure_ollama_client()
+
         try:
-            return client.list()["models"]
+            models = self._ollama_client.list()["models"]
+            Logger().debug(f"Using Ollama server is serving these models: {models}")   
         except httpx.ConnectError:
             Logger().warning(
                 "Ollama server not running, please start the server to use models from Ollama."
@@ -24,6 +39,9 @@ class Ollama:
 
     @classmethod
     def inference(self, model_id: str, prompt: str) -> str:
-        response = client.generate(model=model_id, prompt=prompt.strip())
+        if self._ollama_client is None:
+            self.configure_ollama_client()
+        response = self._ollama_client.generate(
+             model=model_id, prompt=prompt.strip())
 
         return response["response"]
