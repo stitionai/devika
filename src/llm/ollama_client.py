f6e4447cb4fe48368ffa8a81f5cc26e1cd74a395
Dobromir Popov <dobromir.popov@gateway.one>
2024-04-05 00:22:56 +0300

    revert olama changes: functionality already in upstream

--- src/llm/ollama_client.py
+++ src/llm/ollama_client.py
@@ -1,44 +1,27 @@
 import httpx
 from ollama import Client
 from src.config import Config
+
 from src.logger import Logger
 
-log = Logger()
+client = Client(host=Config().get_ollama_api_endpoint())
+
 
 class Ollama:
-    client = None
-    def __init__(self):
+    @staticmethod
+    def list_models():
         try:
-            config = Config()
-            ollama_base = config.get_ollama_api_endpoint()
-            if ollama_base is not None:
-                log.info(f"Using Ollama server at {ollama_base}")    
-                self.client = Client(host=ollama_base)
-            else:
-                log.info(f"Using Ollama server at localhost")    
-                self.client = Client()
-            self.models = self.client.list()["models"]
-            log.info("Ollama available")
-        except:
-            self.client = None
-            log.warning("Ollama not available")
-            log.warning("run ollama server to use ollama models otherwise use other models")
-
-
-    @classmethod
-    def list_models(self):
-        try:
-            models = self.client.list()["models"]
-            log.debug(f"Using Ollama server is serving these models: {models}")   
+            return client.list()["models"]
         except httpx.ConnectError:
-            log.warning(
+            Logger().warning(
                 "Ollama server not running, please start the server to use models from Ollama."
             )
         except Exception as e:
-            log.error(f"Failed to list Ollama models: {e}")
+            Logger().error(f"Failed to list Ollama models: {e}")
+
         return []
-    
-    @classmethod
+
     def inference(self, model_id: str, prompt: str) -> str:
-        response = self.client.generate(model=model_id, prompt=prompt.strip())
+        response = client.generate(model=model_id, prompt=prompt.strip())
+
         return response["response"]
