b5b7194c9c43f309602c1f2ca4e1f1590b4d9df6
Dobromir Popov <d-popov@abv.bg>
2024-04-07 18:21:52 +0300

    fix openai client

--- src/llm/openai_client.py
+++ src/llm/openai_client.py
@@ -1,14 +1,13 @@
-from openai import OpenAI
+from openai import OpenAI as OAI
 
 from src.config import Config
 
-
-class OpenAi:
+class OpenAI:
     def __init__(self):
         config = Config()
         api_key = config.get_openai_api_key()
-        api_base = config.get_openai_api_endpoint()
-        self.client = OpenAI(api_key=api_key,base_url = api_base)
+        api_base = config.get_openai_api_endpoint()        
+        self.client = OAI(api_key=api_key, base_url = api_base)
 
     def inference(self, model_id: str, prompt: str) -> str:
         chat_completion = self.client.chat.completions.create(
@@ -20,4 +19,5 @@ class OpenAi:
             ],
             model=model_id,
         )
+
         return chat_completion.choices[0].message.content
