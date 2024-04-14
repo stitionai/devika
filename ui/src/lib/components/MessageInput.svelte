58d26988c42b771346cc159e909aaeff57fc36c1
Dmitry Vasilev <scythargon@gmail.com>
2024-04-14 11:09:26 +0700

    Update MessageInput.svelte
    Fix typo

--- ui/src/lib/components/MessageInput.svelte
+++ ui/src/lib/components/MessageInput.svelte
@@ -13,7 +13,7 @@
   async function handleSendMessage() {
     const projectName = localStorage.getItem("selectedProject");
     const selectedModel = localStorage.getItem("selectedModel");
-    const serachEngine = localStorage.getItem("selectedSearchEngine");
+    const searchEngine = localStorage.getItem("selectedSearchEngine");
 
     if (!projectName) {
       alert("Please select a project first!");
@@ -31,7 +31,7 @@
           message: messageInput,
           base_model: selectedModel,
           project_name: projectName,
-          search_engine: serachEngine,
+          search_engine: searchEngine,
         });
       } else {
         socket.emit("user-message", { 
@@ -39,7 +39,7 @@
           message: messageInput,
           base_model: selectedModel,
           project_name: projectName,
-          search_engine: serachEngine,
+          search_engine: searchEngine,
         });
       }
       messageInput = "";
