6e00978fa56381d2d10123191380e82e4da9d000
Henri Jamet <henri.jamet@unil.ch>
2024-04-21 00:32:13 +0200

    feat: :construction: better validate_response

--- src/agents/researcher/researcher.py
+++ src/agents/researcher/researcher.py
@@ -1,5 +1,6 @@
 import json
 from typing import List
+import re
 
 from jinja2 import Environment, BaseLoader
 
@@ -18,17 +19,24 @@ class Researcher:
         env = Environment(loader=BaseLoader())
         template = env.from_string(PROMPT)
         return template.render(
-            step_by_step_plan=step_by_step_plan,
-            contextual_keywords=contextual_keywords
+            step_by_step_plan=step_by_step_plan, contextual_keywords=contextual_keywords
         )
 
     def validate_response(self, response: str) -> dict | bool:
         response = response.strip().replace("```json", "```")
 
-        if response.startswith("```") and response.endswith("```"):
-            response = response[3:-3].strip()
+        blocs_json = re.findall(r"{(.*?)}", response, re.DOTALL)
+
+        resultats_json = []
+        for bloc in blocs_json:
+            bloc_nettoye = bloc.replace("\n", "").replace("```\n\n```", "")
+            bloc_json = json.loads("{" + bloc_nettoye + "}")
+            resultats_json.append(bloc_json)
+
+        json_final = json.dumps(resultats_json, indent=4)
+
         try:
-            response = json.loads(response)
+            response = json.loads(json_final)
         except Exception as _:
             return False
 
@@ -37,17 +45,18 @@ class Researcher:
         if "queries" not in response and "ask_user" not in response:
             return False
         else:
-            return {
-                "queries": response["queries"],
-                "ask_user": response["ask_user"]
-            }
+            return {"queries": response["queries"], "ask_user": response["ask_user"]}
 
-    def execute(self, step_by_step_plan: str, contextual_keywords: List[str], project_name: str) -> dict | bool:
-        contextual_keywords_str = ", ".join(map(lambda k: k.capitalize(), contextual_keywords))
+    def execute(
+        self, step_by_step_plan: str, contextual_keywords: List[str], project_name: str
+    ) -> dict | bool:
+        contextual_keywords_str = ", ".join(
+            map(lambda k: k.capitalize(), contextual_keywords)
+        )
         prompt = self.render(step_by_step_plan, contextual_keywords_str)
-        
+
         response = self.llm.inference(prompt, project_name)
-        
+
         valid_response = self.validate_response(response)
 
         while not valid_response:
