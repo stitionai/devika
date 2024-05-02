3b0adb20bfa4acb739106b9456dfda657c2af283
RainRat <rainrat78@yahoo.ca>
2024-05-02 12:44:41 -0700

    fix typos

--- ui/src/lib/components/MessageInput.svelte
+++ ui/src/lib/components/MessageInput.svelte
@@ -20,7 +20,7 @@
   async function handleSendMessage() {
     const projectName = localStorage.getItem("selectedProject");
     const selectedModel = localStorage.getItem("selectedModel");
-    const serachEngine = localStorage.getItem("selectedSearchEngine");
+    const searchEngine = localStorage.getItem("selectedSearchEngine");
 
     if (!projectName) {
       alert("Please select a project first!");
@@ -37,13 +37,13 @@
         message: messageInput,
         base_model: selectedModel,
         project_name: projectName,
-        search_engine: serachEngine,
+        search_engine: searchEngine,
       });
       console.log({ 
         message: messageInput,
         base_model: selectedModel,
         project_name: projectName,
-        search_engine: serachEngine,
+        search_engine: searchEngine,
       });
       messageInput = "";
       
